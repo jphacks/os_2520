6e7ef6233a0cecfce796fbe5894cadbb3f3f781b
Nayuta <nayutafukashigi33@gmail.com>
2025-10-19 10:26:43 +0900

    Yang_dashboardの追加と修正

--- react-vote-app/src/App.tsx
+++ react-vote-app/src/App.tsx
@@ -2,9 +2,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SelectPage1 from "./pages/SelectPage1.tsx";
 import Old from "./pages/Old.tsx";
-import Yang from "./pages/Yang.tsx";
 import OldDashboard from "./pages/Old_dashboard"; // 追加
-
+import Yang from "./pages/Yang.tsx";
+import YangDashboard from "./pages/Yang_dashboard"; // 追加
 
 function App() {
   return (
@@ -14,8 +14,7 @@ function App() {
         <Route path="/old" element={<Old />} />
         <Route path="/old/dashboard" element={<OldDashboard />} /> {/* 追加 */}
         <Route path="/yang" element={<Yang />} />
-
-        
+        <Route path="/yang/dashboard" element={<YangDashboard />} /> {/* 追加 */}     
       </Routes>
     </Router>
   );
